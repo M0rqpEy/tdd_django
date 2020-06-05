1286a67627114e84710e517c7a62ff7866bfca71
M0rqpEy <agaga3041@gmail.com>
2020-06-05 18:51:44 +0300

    added Item model

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
 from django.urls import resolve
 from django.http import HttpRequest
 from django.test import TestCase
+
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 class HomePageTests(TestCase):
@@ -17,3 +19,31 @@ class HomePageTests(TestCase):
         """тест: дом. страница возвращает правильный html"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'lists/home.html')
+
+
+class ItemModelTests(TestCase):
+    """тест модели елемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """тест: сохранения и получения елем. списка"""
+        first_item = Item()
+        first_item.text = 'The first item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
--- tests/check_django.py
+++ tests/check_django.py
@@ -19,8 +19,14 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         """демонтаж"""
-        time.sleep(5)
+        time.sleep(3)
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        """check row in list table """
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retvieve_it_later(self):
         """тест: можно начать список и получить его позже"""
@@ -49,19 +55,20 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows)
-        )
-
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
-        self.fail('Закончить тест')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Сделать мушку из павлиньих перьев')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
+        self.fail('Закончить тест')
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с объяснениями.
