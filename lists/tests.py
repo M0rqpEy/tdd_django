41284860a72f34920fa151362effb8d41e438ea8
M0rqpEy <agaga3041@gmail.com>
2020-06-05 17:11:58 +0300

    Refactoring home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -13,11 +13,7 @@ class HomePageTests(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         """тест: дом. страница возвращает правильный html"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_page(requests):
+def home_page(request):
     """домашня страница"""
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'lists/home.html')
