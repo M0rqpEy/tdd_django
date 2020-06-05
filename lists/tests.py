03b5d4f8ce0ebab223fa1b57f731ba15a5470910
M0rqpEy <agaga3041@gmail.com>
2020-06-05 16:50:20 +0300

    fun.tests added input elem list

--- lists/tests.py
+++ lists/tests.py
@@ -20,4 +20,4 @@ class HomePageTests(TestCase):
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.startswith('</html>'))
+        self.assertTrue(html.endswith('</html>'))
